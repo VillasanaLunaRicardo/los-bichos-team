279e5201dbe2663ee2db74d280da726a48158aab
JORGE GUTIERREZ GARCIA <101275251+JxrgeGtz167@users.noreply.github.com>
2022-05-26 22:03:49 -0500

    Update banner.component.ts

--- proyecto_pag/app/catalogo/banner/banner.component.ts
+++ proyecto_pag/app/catalogo/banner/banner.component.ts
@@ -6,9 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./banner.component.css']
 })
 export class BannerComponent implements OnInit {
-
-  constructor() { }
-
+  id='';
+  constructor(private route:Router, private Ruta:ActivatedRoute) {
+   }
+   OnInit(){
+     this.id=this.Ruta.snapshot.paramMap.get('id')+'';
+   }
+   Mostrarprod(){
+     this.route.navigate(['catalogo/producto'])
+   }
+   mostrardescrip(){
+    this.route.navigate(['catalogo/datalle-prod'])
+   }
+   carrito(){
+    this.route.navigate(['catalogo/carrito'])
+   }
+   iniciosesesion(){
+    this.route.navigate(['catalogo/inicio-ses'])
+   }
+   infotargeta(){
+    this.route.navigate(['catalogo/inf-targeta'])
+   }
+   infoenvio(){
+    this.route.navigate(['catalogo/inf-envio'])
+   }
   ngOnInit(): void {
   }
 
