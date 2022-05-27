57704a1623380da4e1a9325d70c5d3dd6d2ffaec
JORGE GUTIERREZ GARCIA <101275251+JxrgeGtz167@users.noreply.github.com>
2022-05-26 22:06:08 -0500

    Update inf-envio.component.ts

--- proyecto_pag/app/catalogo/inf-envio/inf-envio.component.ts
+++ proyecto_pag/app/catalogo/inf-envio/inf-envio.component.ts
@@ -7,7 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfEnvioComponent implements OnInit {
 
-  constructor() { }
+  id='';
+  constructor(private route:Router, private Ruta:ActivatedRoute) {
+   }
+   OnInit(){
+     this.id=this.Ruta.snapshot.paramMap.get('id')+'';
+   }
+   Mostrarprod(){
+     this.route.navigate(['catalogo/producto'])
+   }
+   mostrardescrip(){
+    this.route.navigate(['catalogo/datalle-prod'])
+   }
+   carrito(){
+    this.route.navigate(['catalogo/carrito'])
+   }
+   iniciosesesion(){
+    this.route.navigate(['catalogo/inicio-ses'])
+   }
+   infotargeta(){
+    this.route.navigate(['catalogo/inf-targeta'])
+   }
+   infoenvio(){
+    this.route.navigate(['catalogo/inf-envio'])
+   }
 
   ngOnInit(): void {
   }
