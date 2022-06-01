0083e65a22db75e84f65ab053f2e0609fc9826b3
SergioAaronValdezMontero <101274912+SergioAaronValdezMontero@users.noreply.github.com>
2022-06-01 17:42:14 -0500

    Add files via upload

---
+++ vistas/src/app/barra/barra.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BarraComponent } from '../barra/barra.component';
+
+
+
+@NgModule({
+  declarations: [
+    BarraComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BarraModule { }
