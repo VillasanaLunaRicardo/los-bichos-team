c0b8b0953723df4d9b79a7df64720bd15b0fba60
VillasanaLunaRicardo <101274884+VillasanaLunaRicardo@users.noreply.github.com>
2022-06-27 23:29:47 -0500

    Create index.js
    en este código hacemos la estipulación de que puerto vamos a ocupar para levantar nuestro servidor, lo que hacemos es seleccionar el puerto, después le indicamos que este escuchando ese puerto para que pueda levantar el servidor y finalmente ponemos un mensaje para saber que este funcionando el servidor y por ultimo creamos una ruta principal para que esa sirva como la ruta fuente y el mismo programa pueda ver en que archivo se encuentran las siguientes rutas con cada una de sus funciones.

---
+++ app/backend/src/index.js
@@ -0,0 +1,9 @@
+const express=require('express');
+const port= (process.env.port || 4200); 
+const app= express();
+
+app.set('port',port);
+app.listen(app.get('port'));
+console.log('servidor funcionando');
+
+app.use('/catalogo',require('./rutas/rutas'));
